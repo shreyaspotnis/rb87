a04e374bd79233df263b74294cec4b9f3ad03171
Shreyas Potnis <shreyaspotnis@gmail.com>
2016-12-18 13:55:47 -0500

    added fundamental constants

--- rb87/__init__.py
+++ rb87/__init__.py
@@ -1,1 +1,14 @@
+
+# Fundamental constants
+pi = 3.1415926
+hbar = 1.054571628e-34  # (SI) Reduced Planck's constant
+h = hbar*2*pi           # (SI) Planck's constant
+kb = 1.3806e-23         # (SI) Boltzman constant
+c = 2.99792458e08       # (m/s) speed of light
+mu0 = 4*pi*1e-07        # (SI) Permeability of Vaccuum
+ep0 = 8.854187e-12      # (SI) Permittivity of Vaccuum
+mub = 9.27400968e-24    # (SI) Bohr magnetron
+g = 9.80665             # (m/s^2) Standard acceleration due to gravity
+
+
 m = 1.443160648e-25         # (kg) Atomic mass of Rubiduim87
